55fd6b490598c5a0bd0ea995a0c4bba971d2cbfe
skoottala <sruthy.koottala@kellogg.northwestern.edu>
2022-02-22 02:09:29 +0000

    post and place views setup

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,38 +1,37 @@
 class PlacesController < ApplicationController
-
     def index
-      @places = Places.all
+        @places =Place.all
     end
-  
+    
     def show
-      @place = Place.find(params["id"])
+        @place = Place.find(params["id"])
     end
-  
+    
     def new
-      @place = Place.new
+        @place = Place.new
     end
-  
+    
     def create
-      @place = Place.new(params["name"])
-      @place.save
-      redirect_to "/places"
+        @place = Place.new(params["name"])
+        @place.save
+        redirect_to "/places"
     end
-  
+    
     def edit
-      @place = Place.find(params["id"])
+        @place = Place.find(params["id"])
     end
-  
+    
     def update
-      @place = Place.find(params["id"])
-      @place.update(params["name"])
-      redirect_to "/places"
+        @place = Place.find(params["id"])
+        @place.update(params["name"])
+        redirect_to "/places"
     end
-  
+    
     def destroy
-      @place = Place.find(params["id"])
-      @place.destroy
-      redirect_to "/places"
+        @place = Place.find(params["id"])
+        @place.destroy
+        redirect_to "/places"
     end
+    
   
-  end
-  
+end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,26 +1,20 @@
 class PostsController < ApplicationController
-  
-    def new
-      @place = Place.new
-    end
-  
-    def create
-      @post = Post.new(params["name"])
-      @post.save
-      redirect_to "/posts"
-    end
-  
-  
-    def update
-      @post = Post.find(params["id"])
-      @post.update(params["name"])
-      redirect_to "/posts"
-    end
-  
-    def destroy
-      @post = Post.find(params["id"])
-      @post.destroy
-      redirect_to "/posts"
-    end
-  
-  end
+    def show
+        @post = Post.find(params["id"])
+      end
+    
+      def new
+        @post = Post.new
+        @place = Place.find(params["place_id"])
+        @post.place_id = @place.id
+      end
+    
+      def create
+        @post = Post.new(params["post"])
+        @post.save
+        redirect_to "/placess/#{@post.place.id}"
+      end
+    
+      
+
+end
